ffd27a7a9c160a6148306df78738ef6b5c7a5e7a
Anusid Wachiracharoenwong <anusid.w@ku.th>
2020-09-15 18:33:11 +0700

    add end_date field to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -12,10 +12,10 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date','end_date']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date', 'was_published_recently','is_published','can_vote')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
