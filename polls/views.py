c8bcb6bac4b28fe802c0560287331caab35f9740
Anusid Wachiracharoenwong <anusid.w@ku.th>
2020-09-17 00:27:05 +0700

    redirect to poll index page

--- polls/views.py
+++ polls/views.py
@@ -85,3 +85,4 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
