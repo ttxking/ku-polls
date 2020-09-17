4fbe1f05933278330027b0bce19995e1ebb34218
Anusid Wachiracharoenwong <anusid.w@ku.th>
2020-09-17 13:36:48 +0700

    improve navigation, apperance and index page

--- polls/admin.py
+++ polls/admin.py
@@ -12,7 +12,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date','end_date']}),
+        ('Date information', {'fields': ['pub_date','end_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently','is_published','can_vote')
--- polls/views.py
+++ polls/views.py
@@ -11,18 +11,22 @@ from django.contrib import messages
 from .models import Question, Choice
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+#     def get_queryset(self):
+#         """
+#         Return the last five published questions (not including those set to be
+#         published in the future).
+#         """
+#         return Question.objects.filter(
+#             pub_date__lte=timezone.now()
+#         ).order_by('-pub_date')[:5]
+
+def index(request) :
+    all_question = Question.objects.all().order_by('-pub_date')
+    return render(request, 'polls/index.html', {'latest_question_list':all_question})
 
 
 # class DetailView(generic.DetailView):
